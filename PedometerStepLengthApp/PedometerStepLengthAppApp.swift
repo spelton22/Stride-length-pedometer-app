fcbf86f30450d981e0d7586becf7fa9b805e3aa9
spelton22 <spelton@chapman.edu>
2025-03-19 11:31:45 -0400

    Initial Commit

---
+++ PedometerStepLengthApp/PedometerStepLengthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedometerStepLengthAppApp.swift
+//  PedometerStepLengthApp
+//
+//  Created by Sophie Pelton on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PedometerStepLengthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
